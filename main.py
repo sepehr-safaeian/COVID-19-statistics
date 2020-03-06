a69818bf134283d5e5f787961147a09e935e1ae0
Sepehr Safaeian <44450634+sepehr-safaeian@users.noreply.github.com>
2020-03-07 02:16:19 +0330

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import requests
+import pandas as pd 
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+
+url = 'https://www.worldometers.info/coronavirus/'
+r = requests.get(url)
+
+data = r.text
+soup = BeautifulSoup(data,'html.parser')
+
+print(soup.title.text)
+print()
+statistics = soup.find_all('div',id='maincounter-wrap')
+for i in statistics:
+    print(i.text)
+
+table_body = soup.find('tbody')
+table_rows = table_body.find_all('tr')
+
+
+countries = []
+cases = []
+todays = []
+deaths = []
+
+for tr in table_rows:
+	td = tr.find_all('td')
+	countries.append(td[0].text)
+	cases.append(td[1].text)
+	todays.append(td[2].text.strip())
+	deaths.append(td[3].text.strip())
+
+indices = [i for i in range(1,len(countries)+1)]
+headers = ['Countries','Total Cases','Todays Cases','Total Deaths']
+df = pd.DataFrame(list(zip(countries,cases,todays,deaths)),index=indices,columns=headers)
+
+print(df)
